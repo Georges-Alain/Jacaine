96ab92060822d81235a57d7aab47e2585ebe2199
Georges-Alain <127270675+Georges-Alain@users.noreply.github.com>
2026-02-01 23:19:29 +0100

    update TimeLineItems Desciption

--- data/timeline.ts
+++ data/timeline.ts
@@ -8,7 +8,7 @@ export const timeline: TimelineItem[] = [
   {
     year: "2005",
     title: "Première Rencontre",
-    description: "Le 18 janvier 2005 au Cameroun à douala plus précisément au carrefour Ari en plein soleil à la sortie de classe, je revenais de l’école et lui il était venu pour un travail (une série EFH) il était cubeur. Je sortais de l’école puis il m’a approché par le biais d’un ami voisin nommé MANG car nous étions dans la même classe et voisin du quartier, lui il vivait à bonadibong. Par l’intermédiaire de MANG il me laissait des messages chaque jour. Il était très attentionné et gentil envers moi. M’invitait toujours à le voir m’envoyant l’argent de transport mais je l’utilisais à d’autres fins. Aux vues de tous ses efforts je décide finalement d’aller chez lui et je ramène mes petites sœurs, histoire de le décourager et de lui montrer que rien ne pouvait se passer. Ce jour il vint me chercher au carrefour Tif et grande est ma surprise il vint avec son ami le nommé MBOT. Mais tout ceci n’a pas changé ma manière de voir les choses. De mon retour de chez eux je ne l'ai jamais rappelé mais malgré cela il m’envoyait toujours des cadeaux et des messages. Cherchant à l’éloigner de moi, je lui ai envoyé une énorme facture de mes produits de beauté et par ma grande surprise il a payé la facture ; c’est ainsi qu’il a commencé à retenir mon attention et quelque temps après la relation a officiellement commencé. En 2008 après la mort de mon père on décide de s’installer ensemble."
+    description: "Le 18 janvier 2005 au Cameroun à douala plus précisément au carrefour Ari en plein soleil à la sortie de classe, je revenais de l’école et lui il était venu pour un travail (une série EFH) il était cubeur. Je sortais de l’école puis il m’a approché par le biais d’un ami voisin nommé MANG. Il vivait à bonadibong. Par l’intermédiaire de MANG, il me laissait des messages chaque jour. Il était très attentionné et gentil envers moi. M’invitait toujours à le voir m’envoyant l’argent de transport mais je l’utilisais à d’autres fins. Aux vues de tous ses efforts je décide finalement d’aller chez lui et je ramène mes petites sœurs, histoire de le décourager et de lui montrer que rien ne pouvait se passer. Ce jour il vint me chercher au carrefour Tif et grande est ma surprise il vint avec son ami le nommé MBOT. Mais tout ceci n’a pas changé ma manière de voir les choses. De mon retour de chez eux je ne l'ai jamais rappelé mais malgré cela il m’envoyait toujours des cadeaux et des messages. Cherchant à l’éloigner de moi, je lui ai envoyé une énorme facture de mes produits de beauté et par ma grande surprise il a payé la facture ; c’est ainsi qu’il a commencé à retenir mon attention et quelque temps après la relation a officiellement commencé. En 2008 après la mort de mon père on décide de s’installer ensemble."
   },
   {
     year: "2009-2012",
@@ -28,6 +28,6 @@ export const timeline: TimelineItem[] = [
   {
     year: "2019",
     title: "Demande en Mariage",
-    description: "En 2019 lors de la grossesse de notre 2eme bébé il fait sa demande en mariage pour m’honorer et me remercier de l’avoir déjà donné notre 1er enfant."
+    description: "En 2019 lors de la grossesse de notre 2eme bébé il fait sa demande en mariage pour m’honorer et me remercier de lui avoir déjà donné notre 1er enfant."
   }
 ];
